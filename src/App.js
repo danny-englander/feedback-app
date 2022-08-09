e045b0e7f4313aca646824a762047ad24640583b
danny-englander <dannyenglander311@gmail.com>
2022-08-09 10:32:25 -0400

    Custom filter function

--- src/App.js
+++ src/App.js
@@ -6,12 +6,26 @@ import FeedbackData from './data/Feebackdata'
 function App() {
   // Pull in the data from the data file.
   const [feedback, setFeedback] = useState(FeedbackData)
+  // Custom function from FeedbackItem > button onClick
+  // to delete the id being clicked on from the JSON array.
+  const deleteFeedBack = (id) => {
+    // Set a custom alert close message, (i.e. if yes)
+    if (window.confirm('Are you sure you want to delete?')) {
+      // This will delete an individual message clicking on a close icon.
+      // .filter is a high order function like .map or a foreach loop.
+      // item.id !== id means the item.id is not equal to the one being passed in marked for deletion.
+      // Thus, it is a fancy way of keeping all ids except the one being clicked on.
+      // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+      // For more info with regard to JS filter
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
 
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedBack} />
       </div>
     </>
   )
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { FaTimes } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from './shared/Card'
 
-const FeedbackItem = ({ item }) => {
+const FeedbackItem = ({ item, handleDelete }) => {
   return (
     // Call the styled component
     <Card reverse={true}>
       <div className="num-display">{item.rating}</div>
+      <button className="close" onClick={() => handleDelete(item.id)}>
+        <FaTimes color="purple" title="Close Icon" />
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   )
